1974b34ad940507d3227f1728cdf81c6d34afa83
Paul Williams <29413800+PaulioRandall@users.noreply.github.com>
2021-06-16 19:12:33 +0100

    Add another test to the slicer

--- pkg/slicer/slicer_test.go
+++ pkg/slicer/slicer_test.go
@@ -16,8 +16,8 @@ func lex(tk token.Token, v string) token.Lexeme {
 }
 
 func TestSliceAll_1(t *testing.T) {
-	// GIVEN two statements as a slice of tokens
-	// WHEN slicing all tokens into statements
+	// GIVEN two statements as a slice of lexemes
+	// WHEN slicing all lexemes into statements
 	// THEN the two statments are separated and returned without error
 
 	lr := NewSliceLexemeReader(
@@ -27,6 +27,8 @@ func TestSliceAll_1(t *testing.T) {
 			lex(token.TokenNumber, "1"),
 			lex(token.TokenNewline, "\n"),
 			lex(token.TokenNumber, "2"),
+			lex(token.TokenNewline, "\n"),
+			lex(token.TokenNumber, "3"),
 		},
 	)
 
@@ -35,6 +37,38 @@ func TestSliceAll_1(t *testing.T) {
 			lex(token.TokenNumber, "1"),
 		},
 		[]token.Lexeme{
+			lex(token.TokenNumber, "2"),
+		},
+		[]token.Lexeme{
+			lex(token.TokenNumber, "3"),
+		},
+	}
+
+	act, e := SliceAll(lr)
+
+	require.Nil(t, e, "%+v", e)
+	require.Equal(t, exp, act)
+}
+
+func TestSliceAll_2(t *testing.T) {
+	// GIVEN a single statement as a slice of lexemes
+	// WHEN slicing all lexemes into statements
+	// THEN the one statments is returned without error
+
+	lr := NewSliceLexemeReader(
+		// 1 + 2
+		// 3 * 4
+		[]token.Lexeme{
+			lex(token.TokenNumber, "1"),
+			lex(token.TokenOperator, "+"),
+			lex(token.TokenNumber, "2"),
+		},
+	)
+
+	exp := [][]token.Lexeme{
+		[]token.Lexeme{
+			lex(token.TokenNumber, "1"),
+			lex(token.TokenOperator, "+"),
 			lex(token.TokenNumber, "2"),
 		},
 	}
