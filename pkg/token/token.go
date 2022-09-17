012c09aa136751c58fafbf91d6ff75fd0b52781a
Paulio <pauliorandall@gmail.com>
2022-09-17 10:34:27 +0100

    Add all keywords to token list

--- pkg/token/token.go
+++ pkg/token/token.go
@@ -5,10 +5,22 @@ type Token int
 const (
 	Unknown Token = iota
 	If
+	For
+	Watch
+	When
+	E
+	F
+	End
 )
 
 var strMap = map[Token]string{
-	If: "if",
+	If:    "if",
+	For:   "for",
+	Watch: "watch",
+	When:  "when",
+	E:     "expression",
+	F:     "function",
+	End:   "end",
 }
 
 func (tk Token) String() string {
@@ -17,7 +29,7 @@ func (tk Token) String() string {
 
 func (tk Token) IsKeyword() bool {
 	switch tk {
-	case If:
+	case If, For, Watch, When, E, F, End:
 		return true
 	default:
 		return false
--- pkg/token/token_test.go
+++ pkg/token/token_test.go
@@ -6,12 +6,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func doTest(t *testing.T, tk Token, isKeyword bool) {
+	require.True(t, len(tk.String()) > 0)
+	require.Equal(t, isKeyword, tk.IsKeyword())
+}
+
 func Test_Unknown(t *testing.T) {
 	require.Equal(t, 0, len(Unknown.String()))
 	require.False(t, Unknown.IsKeyword())
 }
 
-func Test_If(t *testing.T) {
-	require.True(t, len(If.String()) > 0)
-	require.True(t, If.IsKeyword())
-}
+func Test_If(t *testing.T)    { doTest(t, If, true) }
+func Test_For(t *testing.T)   { doTest(t, For, true) }
+func Test_Watch(t *testing.T) { doTest(t, Watch, true) }
+func Test_When(t *testing.T)  { doTest(t, When, true) }
+func Test_E(t *testing.T)     { doTest(t, E, true) }
+func Test_F(t *testing.T)     { doTest(t, F, true) }
+func Test_End(t *testing.T)   { doTest(t, End, true) }
