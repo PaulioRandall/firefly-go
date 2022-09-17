fda37803bf8844a80fef7c1d9e76821b57551f6e
Paulio <pauliorandall@gmail.com>
2022-09-17 09:51:04 +0100

    Remove old implementation files

--- cmd/firefly.go
+++ cmd/firefly.go
@@ -1,40 +1,9 @@
-// Program firefly is a toy programming language built on the following ideas:
-//
-//		Fantasy themed
-//		Specification first
-//		No install
-//		Easily modifiable and extendable (to the extent that is reasonable possible)
 package main
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/PaulioRandall/firefly-go/firefly/firefly"
 )
 
 func main() {
-	file := getFilenameArg()
-	run(file)
-}
-
-func getFilenameArg() string {
-	args := os.Args
-	if len(args) < 2 {
-		panic("expected filename argument")
-	}
-	return args[1]
-}
-
-func run(file string) {
-	e := firefly.RunFile(file)
-	if e != nil {
-		panic(e)
-	}
-}
-
-func printTaskList() {
-	fmt.Print(`
-Hello, firefly!
-`)
+	fmt.Println("TODO: main()")
 }
--- pkg/token/lexeme.go
+++
@@ -1,9 +0,0 @@
-package token
-
-// Lexeme is a container for a value and the token it evaluates to. Sometimes
-// a lexeme will be referred to as a token. This is because the token drives
-// the logic, the value is required only in some cases, e.g. for number tokens.
-type Lexeme struct {
-	Token
-	Value string
-}
