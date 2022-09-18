809e70e4df92e17222481e5672080a25ccef7ba3
Paulio <pauliorandall@gmail.com>
2022-09-19 00:19:27 +0100

    Clean & tidy token types

--- pkg/token/token_type.go
+++ pkg/token/token_type.go
@@ -4,52 +4,55 @@ type TokenType int
 
 const (
 	Unknown TokenType = iota
-	If
+
+	// Keywords
+	If // 1
 	For
 	Watch
 	When
 	E
 	F
 	End
+	True
+	False // 9
+
+	// Variables
 	Var
 )
 
-var strMap = map[TokenType]string{
+var syntaxMap = map[TokenType]string{
 	If:    "if",
 	For:   "for",
 	Watch: "watch",
 	When:  "when",
-	E:     "expression",
-	F:     "function",
+	E:     "E",
+	F:     "F",
 	End:   "end",
+	True:  "true",
+	False: "false",
 	Var:   "variable",
 }
 
-var keywordMap = map[string]TokenType{
-	"if":    If,
-	"for":   For,
-	"watch": Watch,
-	"when":  When,
-	"E":     E,
-	"F":     F,
-	"end":   End,
-}
+var keywords = map[string]TokenType{}
 
-func (tt TokenType) String() string {
-	return strMap[tt]
-}
-
-func (tt TokenType) IsKeyword() bool {
-	switch tt {
-	case If, For, Watch, When, E, F, End:
-		return true
-	default:
-		return false
+func init() {
+	for k, v := range syntaxMap {
+		if k.IsKeyword() {
+			keywords[v] = k
+		}
 	}
 }
 
+func (tt TokenType) IsKeyword() bool {
+	return tt >= 1 && tt <= 9
+}
+
+func (tt TokenType) String() string {
+	return syntaxMap[tt]
+}
+
 func IdentifyWordType(s string) TokenType {
-	k, ok := keywordMap[s]
+	k, ok := keywords[s]
 	if ok {
 		return k
 	}
