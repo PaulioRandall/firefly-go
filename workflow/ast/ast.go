482562ae5f43a3bbff94bd9a2936b313adef0ad2
Paulio <pauliorandall@gmail.com>
2022-09-28 12:31:17 +0100

    Rename Tree to Node to better match graph like nature

--- workflow/ast/ast.go
+++ workflow/ast/ast.go
@@ -1,54 +1,54 @@
 package ast
 
-// Tree represents an AST
+// Node represents an AST
 //
-// Trees maybe nested and dependent upon others in an acyclic manner such that
+// Nodes maybe nested and dependent upon others in an acyclic manner such that
 // at runtime the dependees are executed first and their results used in the
 // parents execution
-type Tree interface {
+type Node interface {
 	Type() ASTType
 	Is(ASTType) bool
 }
 
 // Stmt represents an executable statement
 type Stmt interface {
-	Tree
+	Node
 }
 
 // Bloc represents an executable block of statements
 type Bloc interface {
-	Tree
+	Node
 }
 
 // Proc (Procedure) represents a resolvable expression that can return any
 // number of values including zero
 type Proc interface {
-	Tree
+	Node
 }
 
 // Expr represents a resolvable expression that always returns a single value
 //
-// All sub trees (recursive) of an Expr will also be an Expr or some further
+// All sub nodes (recursive) of an Expr will also be an Expr or some further
 // derived type
 type Expr interface {
-	Tree
+	Node
 }
 
 // MultiExpr represents an Expr that accepts any number of parameters
 type MultiExpr interface {
 	Expr
-	Params() Tree
+	Params() Expr
 }
 
 // BinaryExpr represents an Expr with exactly two parameters
 type BinaryExpr interface {
 	Expr
-	Left() Tree
-	Right() Tree
+	Left() Expr
+	Right() Expr
 }
 
 // UnaryExpr represents an Expr with exactly one parameter
 type UnaryExpr interface {
 	Expr
-	Param() Tree
+	Param() Expr
 }
