1f1dc00d98024c3566be740eca5aad71f54c4d59
Paulio <pauliorandall@gmail.com>
2022-10-06 10:40:33 +0100

    Impl data processor, no tests yet

---
+++ workflow/dataproc/dataproc_test.go
@@ -0,0 +1,24 @@
+package dataproc
+
+/*
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/PaulioRandall/firefly-go/workflow/token"
+)
+
+func tok(tt token.TokenType) token.Token {
+	return token.MakeToken(tt, "", pos.Range{})
+}
+
+func Test_1(t *testing.T) {
+	given := []rune("abc")
+
+	r := inout.NewListReader(given)
+	w := inout.NewListWriter[token.Token]()
+
+	//require.False(t, given.More())
+}
+*/
