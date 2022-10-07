6a7e3f77888109c60a0179aa3490d4e4857d5b5f
Paulio <pauliorandall@gmail.com>
2022-10-07 18:21:14 +0100

    Update parser test + remove obsolete tokenreader pkg

--- workflow/steps/parser/parser_test.go
+++ workflow/steps/parser/parser_test.go
@@ -1,40 +1,40 @@
 package parser
 
-/*
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/PaulioRandall/firefly-go/workflow/tokenreader"
-
 	"github.com/PaulioRandall/firefly-go/workflow/ast"
+	"github.com/PaulioRandall/firefly-go/workflow/inout"
 	"github.com/PaulioRandall/firefly-go/workflow/token"
 
 	"github.com/PaulioRandall/firefly-go/workflow/ast/asttest"
 	"github.com/PaulioRandall/firefly-go/workflow/token/tokentest"
 )
 
-func tok(tt token.TokenType, v string) token.Token {
+func parseTok(tt token.TokenType, v string) token.Token {
 	return tokentest.Tok(tt, v)
 }
 
 func literal(tt token.TokenType, v string) ast.Node {
-	return asttest.Literal(tok(tt, v))
+	return asttest.Literal(parseTok(tt, v))
 }
 
 func assert(t *testing.T, given []token.Token, exp []ast.Node) {
-	tr := tokenreader.FromList(given...)
-	act, e := Parse(tr)
+	r := inout.NewListReader(given)
+	w := inout.NewListWriter[ast.Node]()
+
+	e := Parse(r, w)
 
 	require.Nil(t, e, "%+v", e)
-	require.Equal(t, exp, act)
+	require.Equal(t, exp, w.List())
 }
 
 func Test_1_Compile(t *testing.T) {
 	given := []token.Token{
-		tok(token.Number, "0"),
-		tok(token.Terminator, "\n"),
+		parseTok(token.Number, "0"),
+		parseTok(token.Terminator, "\n"),
 	}
 
 	exp := []ast.Node{
@@ -43,4 +43,3 @@ func Test_1_Compile(t *testing.T) {
 
 	assert(t, given, exp)
 }
-*/
