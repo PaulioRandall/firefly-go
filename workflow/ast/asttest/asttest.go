89e6a38fe5a0c30c2556a863c2c2d1dc8e81d515
Paulio <pauliorandall@gmail.com>
2022-10-10 10:26:41 +0100

    Impl some workflow pkg tests

--- workflow/ast/asttest/asttest.go
+++ workflow/ast/asttest/asttest.go
@@ -1,1 +1,26 @@
 package asttest
+
+import (
+	"github.com/PaulioRandall/firefly-go/workflow/ast"
+	"github.com/PaulioRandall/firefly-go/workflow/token"
+)
+
+func Vars(tks ...token.Token) []ast.Variable {
+	var nodes []ast.Variable
+
+	for _, tk := range tks {
+		nodes = append(nodes, ast.MakeVariable(tk))
+	}
+
+	return nodes
+}
+
+func LitExprs(tks ...token.Token) []ast.Expr {
+	var nodes []ast.Expr
+
+	for _, tk := range tks {
+		nodes = append(nodes, ast.MakeLiteral(tk))
+	}
+
+	return nodes
+}
--- workflow/workflow_test.go
+++ workflow/workflow_test.go
@@ -1,6 +1,5 @@
 package workflow
 
-/*
 import (
 	"testing"
 
@@ -25,17 +24,71 @@ func Test_1_Workflow(t *testing.T) {
 }
 
 func Test_2_Workflow(t *testing.T) {
-	r := inout.NewListReader([]rune("0\n"))
+	// a = 0
+
+	r := inout.NewListReader([]rune("a = 0\n"))
 	w := inout.NewListWriter[ast.Node]()
 
-	e := Parse(r, w)
+	var (
+		gen    = tokentest.NewTokenGenerator()
+		varTk  = gen(token.Var, "a")
+		_      = gen(token.Space, " ")
+		assSym = gen(token.Assign, "=")
+		_      = gen(token.Space, " ")
+		exprTk = gen(token.Number, "0")
+	)
 
-	gen := tokentest.NewTokenGenerator()
 	exp := []ast.Node{
-		asttest.Literal(gen(token.Number, "0")),
+		ast.MakeAssign(
+			assSym,
+			asttest.Vars(varTk),
+			asttest.LitExprs(exprTk),
+		),
 	}
+
+	e := Parse(r, w)
 
 	require.Nil(t, e, "%+v", e)
 	require.Equal(t, exp, w.List())
 }
-*/
+
+func Test_3_Workflow(t *testing.T) {
+	// a, b, c = 0, 1, 2
+
+	r := inout.NewListReader([]rune(`a, b, c = 0, 1, 2` + "\n"))
+	w := inout.NewListWriter[ast.Node]()
+
+	var (
+		gen     = tokentest.NewTokenGenerator()
+		varTk1  = gen(token.Var, "a")
+		_       = gen(token.Comma, ",")
+		_       = gen(token.Space, " ")
+		varTk2  = gen(token.Var, "b")
+		_       = gen(token.Comma, ",")
+		_       = gen(token.Space, " ")
+		varTk3  = gen(token.Var, "c")
+		_       = gen(token.Space, " ")
+		assSym  = gen(token.Assign, "=")
+		_       = gen(token.Space, " ")
+		exprTk1 = gen(token.Number, "0")
+		_       = gen(token.Comma, ",")
+		_       = gen(token.Space, " ")
+		exprTk2 = gen(token.Number, "1")
+		_       = gen(token.Comma, ",")
+		_       = gen(token.Space, " ")
+		exprTk3 = gen(token.Number, "2")
+	)
+
+	exp := []ast.Node{
+		ast.MakeAssign(
+			assSym,
+			asttest.Vars(varTk1, varTk2, varTk3),
+			asttest.LitExprs(exprTk1, exprTk2, exprTk3),
+		),
+	}
+
+	e := Parse(r, w)
+
+	require.Nil(t, e, "%+v", e)
+	require.Equal(t, exp, w.List())
+}
