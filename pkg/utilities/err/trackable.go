c2517f12dc90f67c50ebb4bb07d8a5348d4b339a
Paulio <pauliorandall@gmail.com>
2022-10-23 12:29:59 +0100

    Create trackable error to solve testing & equality issues with errors

---
+++ pkg/utilities/err/trackable.go
@@ -0,0 +1,48 @@
+package err
+
+// trackableError is an error that is tracked through it's error message.
+//
+// This is so that specific error type can be tracked without the underlying
+// cause interfering with equality.
+type trackableError struct {
+	msg   string
+	cause error
+}
+
+// Trackable creates a new template error from which specific errors can be
+// constructed.
+//
+// Call the receiving Track function to create specific instantiations with a
+// cause. The template error must be package scoped such that tests and
+// consuming packages can access the template and check for equality.
+func Trackable(m string) *trackableError {
+	return &trackableError{
+		msg: m,
+	}
+}
+
+// Track returns a trackable error with a cause.
+//
+// errors.Is will return true if the receiving and resultant error are the
+// inputs thus allowing a fairly specific error to be tracked while keeping
+// the cause irrelevant.
+func (e trackableError) Track(cause error) *trackableError {
+	e.cause = cause
+	return &e
+}
+
+func (e *trackableError) Error() string {
+	return e.msg
+}
+
+func (e *trackableError) Unwrap() error {
+	return e.cause
+}
+
+// Is compares only the error message for trackable errors.
+func (e *trackableError) Is(target error) bool {
+	if v, ok := target.(*trackableError); ok {
+		return v.msg == e.msg
+	}
+	return false
+}
